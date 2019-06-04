0cd188c807099be931dabc105f333174432cabc3
ethicalrushi <pupalerusikesh@gmail.com>
2019-06-05 03:44:28 +0530

    Added tests for user and ca models

--- server/settings.py
+++ server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -86,6 +87,9 @@ DATABASES = {
     
 }
 
+if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
+    print('Using sqlite db for testing')
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
---
+++ utils/test_utils.py
@@ -0,0 +1,57 @@
+from users.models import CustomUser, CampusAmbassadorProfile
+
+def create_user(*args,**kwargs):
+    """Create an user with optional kwargs."""
+    create_user.i += 1
+
+    if 'email' not in kwargs:
+        kwargs['email'] = 'email%d@gmail.com' % create_user.i
+    if 'first_name' not in kwargs:
+        kwargs['first_name'] = 'first_name%d' % create_user.i
+    if 'last_name' not in kwargs:
+        kwargs['last_name'] = 'last_name%d' % create_user.i
+    if 'contact_name' not in kwargs:
+        kwargs['contact'] = '1111111111'
+    # Create new object
+    return CustomUser.objects.create(
+        **kwargs
+    )
+
+def save_user(*args,**kwargs):
+    """Saves or modifies an user with optional kwargs."""
+    create_user.i += 1
+
+    if 'email' not in kwargs:
+        kwargs['email'] = 'email%d@gmail.com' % create_user.i
+    if 'first_name' not in kwargs:
+        kwargs['first_name'] = 'first_name%d' % create_user.i
+    if 'last_name' not in kwargs:
+        kwargs['last_name'] = 'last_name%d' % create_user.i
+    if 'contact_name' not in kwargs:
+        kwargs['contact'] = '1111111111'
+    # Create new object
+    user = CustomUser()
+    if 'username' in kwargs:
+        user.username = kwargs['username']
+    user.email = kwargs['email']
+    user.first_name = kwargs['first_name']
+    user.last_name = kwargs['last_name']
+    user.contact = kwargs['contact']
+    user.save()
+    return user
+
+def create_campus_ambassador(*args,**kwargs):
+    """Creates a campus ambassador with optional kwargs."""
+    create_campus_ambassador.i += 1
+
+    if 'user' not in kwargs:
+        kwargs['user'] = create_user()
+    if 'college' not in kwargs:
+        kwargs['college'] = 'college%d' % create_campus_ambassador.i
+    # Create new object
+    return CampusAmbassadorProfile.objects.create(
+        **kwargs
+    )
+
+create_user.i = 0
+create_campus_ambassador.i = 0
