8f2b1e10ddb7265675e276d5f8c5b469ee788b12
Roushan SIngh <38443304+r-tron18@users.noreply.github.com>
2019-06-04 19:46:08 +0530

    updated form to include first and last name
    updated the form so that now it includes first_name and last_name for CustomUserCreationForm
    and also added contact field in CustomUserChangeForm as user might want to change that too

--- users/forms.py
+++ users/forms.py
@@ -6,10 +6,10 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('email','contact','avatar','linkedin','facebook')
+        fields = ('first_name', 'last_name', 'email', 'contact', 'avatar', 'linkedin', 'facebook')
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta(UserChangeForm):
         model = CustomUser
-        fields = ('email','avatar','linkedin','facebook')
+        fields = ('email','avatar','linkedin','facebook', 'contact')
