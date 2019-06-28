fc9c6c988a3d7272dd0b2f1cc8299d060310b182
anshsrtv <ansh_srtv@outlook.com>
2019-06-29 00:40:35 +0530

    users/register added

--- startup/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import startup
-
-class startupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = startup
-        fields = '__all__'
--- startup/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('list/<int:year>/', views.get_startups, name="get-startups"),
-    path('add_new/', views.add_startup, name="add-startup"),
-    path('generate_spreadsheet/', views.generate_spreadsheet, name="startup-gen-csv"),
-]
