592c8af68a33ea0330617fe9471a01d752e137fa
anshsrtv <49693160+anshsrtv@users.noreply.github.com>
2019-06-29 16:49:49 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -67,8 +67,8 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'test': {
-          'ENGINE': 'django.db.backends.sqlite3',
-          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
