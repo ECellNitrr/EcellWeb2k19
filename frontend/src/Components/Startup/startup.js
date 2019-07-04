fb3a17ae3ad506954f271f824461891583f9d174
pratik0204 <46794833+pratik0204@users.noreply.github.com>
2019-07-04 17:09:52 +0530

    resolved conflicts

--- frontend/src/Components/Startup/startup.js
+++ frontend/src/Components/Startup/startup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{Component} from 'react';
 import faxios from '../../axios';
 import './startup.css';
@@ -17,19 +16,4 @@ class startup extends Component{
         )
     }
 }
-
-=======
-import React from 'react';
-import './startup.css';
-import Navbar from '../Navbar/navbar';
-
-const startup=()=>(
-    <div>
-        <Navbar/>
-        <div className="startup">
-            Site Under Construction
-        </div>
-    </div>
-)
->>>>>>> ecell/master
 export default startup;
