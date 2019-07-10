4c98921180022af03721570ac2c40d3cf00c59d5
Aryan Sarkar <aryansarkar13@users.noreply.github.com>
2019-07-11 01:31:15 +0530

    Events page
    Links to be added.

--- frontend/src/Components/Events/events.js
+++ frontend/src/Components/Events/events.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import faxios from '../../axios';
+import ReactDOM from "react-dom";
 import './events.css';
 import Navbar from '../Navbar/navbar';
 
@@ -11,8 +12,94 @@ class events extends Component{
             <div>
                 <Navbar/>
                 <div className="events">
-                Site Under Construction
-                </div>
+                    <br/><br/><br/>
+                
+      <div id="event1" class="event1">
+        <div class="container">
+          <h2>Our EVENTS</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est
+            facilis maiores, perspiciatis accusamus asperiores sint consequuntur
+            debitis.
+          </p>
+		  <div class="row">
+		  <div class="col-sm-4">
+
+        
+            <div class="icons">
+              <figure>
+               <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/1.png")} alt="user" /></a>
+              </figure>
+              <span>Ignition</span>
+            </div>
+          
+            <div class="icons">
+              <figure>
+              <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/2.png")} alt="user" /></a>
+              </figure>
+              <span>Startup Camp</span>
+            </div>
+         
+            <div class="icons">
+              <figure>
+              <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/3.png")} alt="user" /></a>
+              </figure>
+              <span>Cricnometrica</span>
+            </div>
+			</div>
+			
+            <div class="col-sm-4">
+          
+              <div class="icons">
+                <figure>
+                <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/4.png")} alt="user" /></a>
+                </figure>
+                <span>B-Quiz</span>
+              </div>
+           
+              <div class="icons">
+                <figure>
+                <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/5.png")} alt="user" /></a>
+                </figure>
+                <span>Wall Street</span>
+              </div>
+           
+              <div class="icons">
+                <figure>
+                <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/6.png")} alt="user" /></a>
+                </figure>
+                <span>Uthkristh</span>
+              </div>
+			 </div>
+			<div class="col-sm-4">
+            
+            <div class="icons">
+              <figure>
+              <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/7.png")} alt="user" /></a>
+              </figure>
+              <span>Entropy</span>
+            </div>
+          
+            <div class="icons">
+              <figure>
+              <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/8.png")} alt="user" /></a>
+              </figure>
+              <span>E - Gathering</span>
+            </div>
+          
+            <div class="icons">
+              <figure>
+              <a href="/event/${event.id}" class="register_btn" data-eid="${event.id}"><img src={require("../../assets/events/9.png")} alt="user" /></a>
+              </figure>
+              <span>B-Case Study</span>
+            </div>
+			</div>
+			</div>
+          
+        </div>
+      </div>
+    </div>
+                
             </div>
         )
     }
