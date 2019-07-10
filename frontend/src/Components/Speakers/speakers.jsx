187a4679f78d8ac8262abfb80d29b2d3f32a2af6
Aryan Sarkar <aryansarkar13@users.noreply.github.com>
2019-07-11 02:04:25 +0530

    Restored speakers and leaderboard.
    Somebody deleted them causing problems.

---
+++ frontend/src/Components/Speakers/speakers.jsx
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+import faxios from '../../axios';
+import './speakers.css';
+import Navbar from '../Navbar/navbar';
+
+class speakers extends Component{
+
+    render(){
+        return(
+            <div>
+                <Navbar/>
+                <div className="speakers">
+                Site Under Construction
+                </div>
+            </div>
+        )
+    }
+}
+export default speakers;
