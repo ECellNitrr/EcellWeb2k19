62f62108d8ff85005fecdf4d47ae98781ca48d1a
ethicalrushi <pupalerushikesh@gmail.com>
2019-07-11 14:40:29 +0530

    added unique constraint in mentor name, using python 3.6 in travis

--- mentors/models.py
+++ mentors/models.py
@@ -3,7 +3,7 @@ from users.models import CustomUser
 
 
 class Mentor(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, unique=True)
     contact = models.TextField(max_length=13, null=True, blank=True)
     email = models.CharField(max_length=256)
     detail = models.TextField()
@@ -14,11 +14,9 @@ class Mentor(models.Model):
         blank=True)
     flag = models.BooleanField(default=False)
     year = models.IntegerField(default=2019)
-    ecell_user = models.ForeignKey(
-        CustomUser,
-        on_delete=models.SET_NULL, null=True, blank=True)
+    ecell_user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
-    
+
     def __str__(self):
         return self.name
