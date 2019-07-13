f553563cb4584eee007f7f023857b6f6fed6d9fc
Aryan Sarkar <aryansarkar13@users.noreply.github.com>
2019-07-13 22:56:42 +0530

    Added the faculty members in the team page.
    Styling is still needed.

--- frontend/src/Components/Team/faculty.jsx
+++ frontend/src/Components/Team/faculty.jsx
@@ -2,25 +2,22 @@ import React,{Component} from 'react';
 import faxios from '../../axios';
 import "./team.css";
 import Navbar from '../Navbar/navbar';
-import Faculty from './faculty';
 
-class team extends Component{
+class faculty extends Component{
     axios = faxios();
     state = {
-    student: {},
     faculty: {},
     loading: true
   };
   componentDidMount() {
     this.axios.get("/team/list/").then(fromserver => {
       const data = fromserver.data;
-      const student = data.student;
       const faculty = data.faculty;
       const members = {}
 
       let member_types = [];
-      for(const x in student){
-          const member_type = student[x].member_type
+      for(const x in faculty){
+          const member_type = faculty[x].member_type
           if(member_types.indexOf(member_type)===-1){
               member_types.push(member_type)
           }
@@ -29,48 +26,29 @@ class team extends Component{
 
       for(const x in member_types){
           const member_type = member_types[x]
-          members[member_type] = student.filter(member => member.member_type===member_type)
+          members[member_type] = faculty.filter(member => member.member_type===member_type)
       }
 
       console.log(members)
 
       console.log(data);
       this.setState({
-        student: members,
+        faculty: members,
         loading: false
       });
     });
   }
 
   render() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 6,
-        autoplay:true,
-        autoplaySpeed:3000,
-        slidesToScroll: 6,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 4,
-              slidesToScroll: 4,
-              infinite: true
-            }
-          }
-        ]
-      };
-      console.log(this.state.student)
+    
+      console.log(this.state.faculty)
     let team_html = []
-    for(const member_type in this.state.student){
-        let student = this.state.student[member_type]
-        if(member_type==="OC")
+    for(const member_type in this.state.faculty){
+        let faculty = this.state.faculty[member_type]
+        if(member_type==="HCD")
         {
             
-            student = student.map(member => 
+            faculty = faculty.map(member => 
                 <div>
                 <div>
                 <img src={member.image} alt={member.name} width="200" height="200"/>
@@ -86,10 +64,10 @@ class team extends Component{
         const teams_html = <div key={member_type}>
             
             <div>
-            <h2>Overall Coordinators</h2>
+            <h2>Head of Career Development Cell</h2>
             </div>
             <div className="flex-container">
-            {student}
+            {faculty}
             </div>
             
            
@@ -101,10 +79,10 @@ class team extends Component{
         
             
         }
-        else if(member_type==="HC")
+        else if(member_type==="Dir")
         {
             
-            student = student.map(member => 
+            faculty = faculty.map(member => 
                 
                 <div>
                 <div>
@@ -119,12 +97,12 @@ class team extends Component{
         
         const teams_html = <div key={member_type}>
 
-            
+            <br/><br/><br/>
             <div className="flex-container">
-            <span><h2>Head Coordinators</h2></span>
+            <span><h2>Director NIT Raipur</h2></span>
             </div>
             <div className="flex-container">
-            {student}
+            {faculty}
             </div>
            
             
@@ -135,28 +113,32 @@ class team extends Component{
         
             
         }
-        else if(member_type==="MNG")
+        else
         {
             
-            student = student.map(member => 
-                
+            faculty = faculty.map(member => 
                 <div>
+                <div>
+                <img src={member.image} alt={member.name} width="200" height="200"/>
+                </div>
                 <div>
                 <h6>{member.name}</h6>
                 </div>
                 </div>
+               
                     
             )
         
         const teams_html = <div key={member_type}>
-
             
-            <div className="flex-container">
-            <span><h2>Managers</h2></span>
+            <div>
+            
+            <h2>Faculty Incharge</h2>
             </div>
             <div className="flex-container">
-            {student}
+            {faculty}
             </div>
+            
            
             
             
@@ -166,51 +148,21 @@ class team extends Component{
         
             
         }
-        else{
-
+        
+        
        
-        
-        student = student.map(member => 
-                
-            <div>
-            <div>
-            <h6>{member.name}</h6>
-            </div>
-            </div>
-                
-                    
-            )
-        
-        const teams_html = <div key={member_type}>
-
-            <div className="flex-container">
-            <span><h2>Executives</h2></span>
-            </div>
-            <div className="flex-container">
-            {student}
-            </div>
-        
-           
-            
-            
-        </div>
-        
-        team_html.push(teams_html)
-        }
     }
         return(
             <div>
                 <Navbar/>
-                <Faculty/>
-                <div className="team">{this.state.loading? 'Loading...':team_html[0]}</div>
-                <div className="team">{this.state.loading? 'Loading...':team_html[1]}</div>
+                
                 <div className="team">{this.state.loading? 'Loading...':team_html[2]}</div>
-                <div className="team">{this.state.loading? 'Loading...':team_html[3]}</div>
-
+                <div className="team">{this.state.loading? 'Loading...':team_html[1]}</div>
+                <div className="team">{this.state.loading? 'Loading...':team_html[0]}</div>
             </div>
         )
     }
 }
 
-export default team;
+export default faculty;
 
