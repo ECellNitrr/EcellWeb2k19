e3695b5a9addd318a3127b683ddfdc0c380da7eb
naveensundar <naveennvrgup@gmail.com>
2019-07-14 12:06:05 +0530

    speakers mentors and sponsors pages done

--- frontend/src/Components/Sponsors/header_links.js
+++ frontend/src/Components/Sponsors/header_links.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {BrowserRouter,Route,Switch,Redirect} from 'react-router-dom';
-import yearwise from '../Sponsors/yearwise_sponsors';
 
 const links =()=>{
     return(
