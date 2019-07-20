c7863bfa380156b572236c97a4b895e437ef7e2c
ethicalrushi <pupalerushikesh@gmail.com>
2019-07-21 01:06:58 +0530

    gloabl error messages for decorators

--- decorators.py
+++ decorators.py
@@ -4,21 +4,20 @@ from users.models import CustomUser
 from rest_framework import status
 from rest_framework.response import Response
 
+NO_TOKEN = Response({
+                "message":"No Token Provided!"
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+DOES_NOT_EXIST = Response({
+                    "message":"User does not exist!"
+            }, status=status.HTTP_404_NOT_FOUND)
+
+ACCESS_ERROR = Response({
+                    "message": "You are not authorized to use this API"
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
 def ecell_user(function):
     def wrap(request, *args, **kwargs):
-
-        NO_TOKEN = Response({
-                        "message":"No Token Provided!"
-                    }, status=status.HTTP_401_UNAUTHORIZED)
-        
-        DOES_NOT_EXIST = Response({
-                            "message":"User does not exist!"
-                    }, status=status.HTTP_404_NOT_FOUND)
-        
-        ACCESS_ERROR = Response({
-                            "message": "You are not authorized to use this API"
-                    }, status=status.HTTP_401_UNAUTHORIZED)
-
         token = request.META.get("HTTP_AUTHORIZATION", None)
         if token is not None: 
             try:
@@ -44,23 +43,11 @@ def ecell_user(function):
 
 def client_check(function):
     def wrap(request, *args, **kwargs):
-
-        NO_TOKEN = Response({
-                        "message":"No Token Provided!"
-                    }, status=status.HTTP_401_UNAUTHORIZED)
-        
-        
-        ACCESS_ERROR = Response({
-                            "message": "You are not authorized to use this API"
-                    }, status=status.HTTP_401_UNAUTHORIZED)
-
         token = request.META.get("HTTP_ACCESS", None)
-        print(token)
         if token is not None: 
             try:
                 payload = jwt.decode(token, config('SECRET_KEY'))
             except Exception as e:
-                print(e)
                 return ACCESS_ERROR
             else:
                 print(payload)
