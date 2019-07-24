10dcf3c3813275d2bf00bc82b5e28fb51aaf5cb8
naveensundar <naveennvrgup@gmail.com>
2019-07-24 09:26:29 +0530

    merged with master

--- ca_portal/migrations/0002_ca_requests_user.py
+++ ca_portal/migrations/0002_ca_requests_user.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('sponsors', '0001_initial'),
+        ('ca_portal', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='sponsor',
-            name='ecell_user',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL),
+            model_name='ca_requests',
+            name='user',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='requests', to=settings.AUTH_USER_MODEL),
         ),
     ]
