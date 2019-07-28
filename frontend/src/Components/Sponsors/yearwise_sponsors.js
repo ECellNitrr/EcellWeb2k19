41519a33d7f755259c4bef78a4523ad5236f2bd4
pratik0204 <pchy.393@gmail.com>
2019-07-28 16:35:54 +0530

    yearwise_spons done

--- frontend/src/Components/Sponsors/yearwise_sponsors.js
+++ frontend/src/Components/Sponsors/yearwise_sponsors.js
@@ -3,29 +3,50 @@ import {Link} from 'react-router-dom';
 import './sponsors.css';
 import Navbar from '../Navbar/navbar';
 import Footer from '../Footer/footer';
+import faxios from '../../axios';
+
 
 
 class Sponsors extends Component{
     
+    axios = faxios()
+    state={
+        spons_years:[]
+    }
+
+    componentDidMount(){
+        this.axios.get('/sponsors/spons_years/').then(res=>{
+            
+            let data = res.data.spons_year;
+            console.log(data);
+            this.setState({
+                spons_years:data
+            })
+        })
+    }
     
 
     render(){
+
+        console.log(this.state)
+        let spons_year_html= this.state.spons_years.map(year=>{
+            return <div key={year}><Link to={`/sponsors/${year}`}><button className="btn" >Sponsors {year}</button></Link></div> 
+        })
         
         return(
             <div className="sponsors">
                 <Navbar/>
+
                 <div className="header1">OUR SPONSORS</div>
                 <div className="container-fluid ctn11">
                     <div>
-                        <div><Link to="/sponsors/2019" ><button className="btn" >Sponsors 2019</button></Link></div>
-                        <div><Link to="/sponsors/2018" ><button className="btn" >Sponsors 2018</button></Link></div>
-                        <div><Link to="/sponsors/2017" ><button className="btn" >Sponsors 2017</button></Link></div>
-                        <div><Link to="/sponsors/2016" ><button className="btn" >Sponsors 2016</button></Link></div>
-                        <div><Link to="/sponsors/2015" ><button className="btn" >Sponsors 2015</button></Link></div>
+                        {/*<div><Link to="/sponsors/2019" ><button className="btn" >Sponsors 2019</button></Link></div>*/}
+                        {spons_year_html}
+                        
                     </div>
                 </div>
 
-                <h2 className="header3">SPONSORSHIP GALLERY</h2>
+                {/*<h2 className="header3">SPONSORSHIP GALLERY</h2>
                 <div className="container-fluid ctn15">
                     <div>
                         <div><a href="#"><button className="btn" >Sponsors Of E-Summit'19</button></a></div>
@@ -34,7 +55,7 @@ class Sponsors extends Component{
                         <div><a href="#"><button className="btn" >Sponsors Of E-Summit'16</button></a></div>
                         <div><a href="#"><button className="btn" >Sponsors Of E-Summit'15</button></a></div>
                     </div>
-                </div>
+        </div>*/}
                 
                 
                 <Footer/>
