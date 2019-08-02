2e3f7f4736357d8461a1e3e584a85d2a9e3b3996
pratik0204 <46794833+pratik0204@users.noreply.github.com>
2019-08-02 21:27:54 +0530

    Team Page Redone. (#59)
    * team page redone
    
    * removed style errors
    
    * team page done
    
    * removed event-register button temporarily

--- frontend/src/Components/Team/team.js
+++ frontend/src/Components/Team/team.js
@@ -5,6 +5,9 @@ import Navbar from "../Navbar/navbar";
 // import Faculty from "./faculty";
 import Footer from "../Footer/footer";
 import Loader from '../api_loader/api_loader'
+import {Link} from 'react-router-dom';
+
+
 
 class team extends Component {
     axios = faxios();
@@ -13,7 +16,8 @@ class team extends Component {
         loading: true
     };
     componentDidMount() {
-        this.axios.get("/team/list/2018/").then(fromserver => {
+        const year = this.props.match.params.year;
+        this.axios.get(`/team/list/${year}/`).then(fromserver => {
             const data = fromserver.data.data;
             console.log(data)
             /*delete data["me"];*/
@@ -24,8 +28,16 @@ class team extends Component {
                 Faculty_Incharge:[],
                 Overall_Coordinator:[],
                 Head_Coordinator:[],
-                Manager:[],
-                Executive:[]       
+                Manager_pr:[],
+                Manager_tech:[],
+                Manager_des:[],
+                Manager_doc:[],
+                Manager_spons:[],
+                Executive_pr:[],
+                Executive_tech:[],
+                Executive_des:[],
+                Executive_doc:[],
+                Executive_spons:[]     
             }
 
             data.forEach(members => {
@@ -43,18 +55,51 @@ class team extends Component {
 
                 if(members.member_type==="OCO"){
                     members_dict["Overall_Coordinator"].push(members)
+                    
                 }
 
                 if(members.member_type==="HCO"){
                     members_dict["Head_Coordinator"].push(members)
                 }
 
-                if(members.member_type==="MNG"){
-                    members_dict["Manager"].push(members)
+                if(members.member_type==="MNG" && members.domain==="pr"){
+                    members_dict["Manager_pr"].push(members)
                 }
 
-                if(members.member_type==="EXC"){
-                    members_dict["Executive"].push(members)
+                if(members.member_type==="MNG" && members.domain==="tech"){
+                    members_dict["Manager_tech"].push(members)
+                }
+
+                if(members.member_type==="MNG" && members.domain==="design"){
+                    members_dict["Manager_des"].push(members)
+                }
+
+                if(members.member_type==="MNG" && members.domain==="doc"){
+                    members_dict["Manager_doc"].push(members)
+                }
+
+                if(members.member_type==="MNG" && members.domain==="spons"){
+                    members_dict["Manager_spons"].push(members)
+                }
+
+                if(members.member_type==="EXC" && members.domain==="pr"){
+                    members_dict["Executive_pr"].push(members)
+                }
+
+                if(members.member_type==="EXC" && members.domain==="tech"){
+                    members_dict["Executive_tech"].push(members)
+                }
+
+                if(members.member_type==="EXC" && members.domain==="design"){
+                    members_dict["Executive_des"].push(members)
+                }
+
+                if(members.member_type==="EXC" && members.domain==="doc"){
+                    members_dict["Executive_doc"].push(members)
+                }
+
+                if(members.member_type==="EXC" && members.domain==="spons"){
+                    members_dict["Executive_spons"].push(members)
                 }
 
 
@@ -69,37 +114,67 @@ class team extends Component {
         });
     }
 
+    
+    
+
     members_html = (title, members, type) => {
         members = members.map(member => (
             <div key={member.id} className='text-center'>
                 <div>
-                    {title === "Managers" || title === "Executives" ? null : (
-                        <img
-                            className="member-image shadow-lg p-3 mb-5 bg-white rounded"
-                            src={member.image}
-                            alt={member.name}
-                            width="250"
-                            height="250"
-                        />
-                    )}
-                </div>
-                <div>
-                    <h6 className="member-name">{member.name}</h6>
+                    {title === "Head_Career_Development" ||title === "Director" ||title === "Faculty Incharge" ||title === "Overall Co-ordinators" ||title === "Head Co-ordinators" ? (
+                    <div>
+                            <div>
+                                <img
+                                className="member-image shadow-lg p-3 mb-5 bg-white rounded"
+                                src={member.image}
+                                alt={member.name}
+                                width="270"
+                                height="270"
+                                ></img>
+                                
+                            </div>
+                        <div>
+                            <h6 className="member-name">{member.name}</h6>
+                        </div>
+
+                        <div className="domain">
+                            {member.domain==="pr"?<p style={{fontSize:"20px",marginBottom:"25px",marginTop:"-25px"}}><i>(Public Relation and Marketing)</i></p>:null}
+                            {member.domain==="tech"?<p style={{fontSize:"20px",marginBottom:"25px",marginTop:"-25px"}}><i>(Technical Team)</i></p>:null}
+                            {member.domain==="design"?<p style={{fontSize:"20px",marginBottom:"25px",marginTop:"-25px"}}><i>(Design Team)</i></p>:null}
+                            {member.domain==="spons"?<p style={{fontSize:"20px",marginBottom:"25px",marginTop:"-25px"}}><i>(Sponsorship Team)</i></p>:null}
+                            {member.domain==="doc"?<p style={{fontSize:"20px",marginBottom:"25px",marginTop:"-25px"}}><i>(Documentation Team)</i></p>:null}
+                        </div>
+                    </div>
+                        
+                    ):<div>
+                        <h6 className="member-name">{member.name}</h6>
+                     </div>}
                 </div>
             </div>
         ));
 
         return (
             <div key={type}>
-                <h2 className="position shadow p-3 mb-5 bg-white rounded">
-                    {title}
-                </h2>
-                <div className="flex-container">{members}</div>
+                
+                {title === "Head_Career_Development" ||title === "Director" ||title === "Faculty Incharge" ||title === "Overall Co-ordinators" ||title === "Head Co-ordinators" ? (
+                    <h2 style={{marginTop:"70px"}} className="position shadow p-3 mb-5 bg-white rounded">
+                        {title}
+                    </h2>
+                ):(
+                    <h2 style={{marginTop:"70px"}} className="position-exec shadow p-3 mb-5 bg-white rounded">
+                        {title}
+                    </h2>
+                )}
+                
+                <div className="flex-containerrr justify-content-center">{members}</div>
             </div>
         );
     };
 
     render() {
+
+       
+
         let final_html = {};
 
         for (let type in this.state.members) {
@@ -141,33 +216,95 @@ class team extends Component {
                         type
                     );
                     break;
-                case "Manager":
-                    final_html["Manager"] = this.members_html(
-                        "Managers",
+                case "Manager_pr":
+                    final_html["Manager_pr"] = this.members_html(
+                        "Public Relation and Marketing Managers",
                         members,
                         type
                     );
                     break;
-                case "Executive":
-                    final_html["Executive"] = this.members_html("Executives", members);
+                case "Manager_tech":
+                    final_html["Manager_tech"] = this.members_html(
+                        "Technical Team Managers ",
+                        members,
+                        type
+                    );
+                    break;
+
+                case "Manager_des":
+                    final_html["Manager_des"] = this.members_html(
+                        "Design Team Managers ",
+                        members,
+                        type
+                    );
+                    break;
+
+                case "Manager_doc":
+                    final_html["Manager_doc"] = this.members_html(
+                        "Documentation Team Manager ",
+                        members,
+                        type
+                    );
+                    break;
+
+                case "Manager_spons":
+                    final_html["Manager_spons"] = this.members_html(
+                        "Sponsorship and Brand Management Managers ",
+                        members,
+                        type
+                    );
+                    break;
+                case "Executive_pr":
+                    final_html["Executive_pr"] = this.members_html("Public Relation and Marketing Executives", members);
+                    break;
+
+                case "Executive_tech":
+                    final_html["Executive_tech"] = this.members_html("Technical Team Executives", members);
+                    break;
+
+                case "Executive_des":
+                    final_html["Executive_des"] = this.members_html("Design Team Executives", members);
+                    break;
+
+                case "Executive_doc":
+                    final_html["Executive_doc"] = this.members_html("Documentation Team Executives", members);
+                    break;
+
+                case "Executive_spons":
+                    final_html["Executive_spons"] = this.members_html("Sponsorship and Brand Management Executives", members);
                     break;
                 default:
                     console.log("default");
             }
         }
 
+        let this_year=this.props.match.params.year;
         // final_html = Object.keys(final_html).map(key => final_html[key]);
 
         return (
             <div className="team-whole">
                 <Navbar />
-                {this.state.loading ? (<div style={{marginTop:"10%"}}><Loader/></div>):(<div>{final_html['Director']}
-                {final_html['Head_Career_Development']}
-                {final_html['Faculty_Incharge']}
-                {final_html['Overall_Coordinator']}
+                <div className="team-div"><Link className="team-links shadow p-3 mb-5 bg-white rounded" to='/team/yearwise'>Previous Year Teams</Link></div>
+                {this.state.loading ? (<div style={{marginTop:"10%"}}><Loader/></div>):(
+                <div>
+                
+                {this_year==="2019" ? final_html['Director']:null}
+                {this_year==="2019" ? final_html['Head_Career_Development']:null}
+                {this_year==="2019" ? final_html['Faculty_Incharge']:null}
+                {this_year==="2013" ? null :final_html['Overall_Coordinator']}
                 {final_html['Head_Coordinator']}
-                {final_html['Manager']}
-                {final_html['Executive']}</div>) }
+                {this_year==="2019" ? final_html['Manager_pr']:null}
+                {this_year==="2019" ? final_html['Manager_tech']:null}
+                {this_year==="2019" ? final_html['Manager_des']:null}
+                {this_year==="2019" ? final_html['Manager_doc']:null}
+                {this_year==="2019" ? final_html['Manager_spons']:null}
+                {this_year==="2019" ? final_html['Executive_pr']:null}
+                {this_year==="2019" ? final_html['Executive_tech']:null}
+                {this_year==="2019" ? final_html['Executive_des']:null}
+                {this_year==="2019" ? final_html['Executive_doc']:null}
+                {this_year==="2019" ? final_html['Executive_spons']:null}
+                </div>) }
+                <div className="team-div"><Link className="team-links shadow p-3 mb-5 bg-white rounded" to='/team/yearwise'>Previous Year Teams</Link></div>
                 <Footer />
             </div>
         );
---
+++ frontend/src/Components/Team/team_list.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import faxios from '../../axios';
+import Navbar from '../Navbar/navbar';
+import Footer from '../Footer/footer';
+import Loader from '../api_loader/api_loader'
+import './team.css'
+
+export default class team_list extends Component {
+
+    state={
+        team_years:[],
+        loading:true
+    }
+
+    componentDidMount(){
+        faxios().get('team/team_years/').then(res=>{
+            
+            let data=res.data.years
+            let team_extract=[]
+
+            data.forEach(el => {
+                let year = el[0];
+                team_extract.push(year);
+            });
+            let year_sorted=team_extract.sort().reverse();
+            
+            this.setState({
+                team_years:year_sorted,
+                loading:false
+            })
+        })
+    }
+
+    render() {
+        
+        let team_year_html= this.state.team_years.map(year=>{
+            return <div key={year}><Link to={`/team/${year}`}><button className="btn" >Team of {year}</button></Link></div> 
+        })
+
+        return (
+            <div className="team-whole">
+                <Navbar/>
+
+                <div className="header1">TEAM E-CELL</div>
+                <div className="container-fluid ctn11">
+                        <div style={{marginTop:"-250px"}}>
+                            {this.state.loading ? (<Loader/>):(team_year_html) }
+                        </div>
+                </div>    
+                
+                <Footer/>
+            </div>
+        )
+    }
+}
