981e9d2eb47d95ea731f5eebaf1e771014d7bf13
Aryan Sarkar <aryansarkar13@users.noreply.github.com>
2019-08-03 02:29:45 +0530

    Timeline finished.

--- frontend/src/Components/Home/Timeline/timeline.jsx
+++ frontend/src/Components/Home/Timeline/timeline.jsx
@@ -2,48 +2,86 @@ import React from 'react';
 import './timeline.css';
 const timeline = ()=>{
     return(
-        <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="main-timeline">
-                <div class="timeline">
-                    <span class="icon">2019</span>
-                    <div class="timeline-content">
-                        <h3 class="title">E-Summit</h3>
-                        <p class="description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem, et condimentum neque commodo quis.
+        <div className="padd">
+        <div className="heading">
+        <h2>Timeline</h2>
+        </div>
+        <div className="container">
+    <div className="row">
+        <div className="col-md-12">
+            <div className="main-timeline">
+                <div className="timeline">
+                    <span className="icon">2019</span>
+                    <div className="timeline-content">
+                        <h3 className="title">E-Summit 2019</h3>
+                        <p className="description">
+                            COMING SOON.
                         </p>
                     </div>
                 </div>
-                <div class="timeline">
-                    <span class="icon">2018</span>
-                    <div class="timeline-content">
-                        <h3 class="title">E-Summit</h3>
-                        <p class="description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem, et condimentum neque commodo quis.
+                <div className="timeline">
+                    <span className="icon">2018</span>
+                    <div className="timeline-content">
+                        <h3 className="title">Merge of E-Cell And I-Cell</h3>
+                        <p className="description">
+                        E-Cell along with the Innovation Cell became part of a new initiative taken in the
+direction of career and placements, i.e. Career Development Cell. This body governs all
+the career and student related activities and stresses the need of progressive
+development of the students.
                         </p>
                     </div>
                 </div>
-                <div class="timeline">
-                    <span class="icon">2017</span>
-                    <div class="timeline-content">
-                        <h3 class="title">E-Summit</h3>
-                        <p class="description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem, et condimentum neque commodo quis.
+                <div className="timeline">
+                    <span className="icon">2018</span>
+                    <div className="timeline-content">
+                        <h3 className="title">E-Cell's debut on Instagram</h3>
+                        <p className="description">
+                        E-Cell, NIT Raipur has been successful in approaching thousands of people via different
+social media platforms. Considering the fact that a large fraction of youth is present on
+Instagram, E-Cell made its debut on this popular social platforms, providing more
+graphic information about the events and programmes.
                         </p>
                     </div>
                 </div>
-                <div class="timeline">
-                    <span class="icon">2016</span>
-                    <div class="timeline-content">
-                        <h3 class="title">E-Summit</h3>
-                        <p class="description">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer malesuada tellus lorem, et condimentum neque commodo quis.
+                <div className="timeline">
+                    <span className="icon">2018</span>
+                    <div className="timeline-content">
+                        <h3 className="title">International speaker at Entropy</h3>
+                        <p className="description">
+                        Every year, team E-Cell, NIT Raipur brings best of the speakers and entrepreneurs to
+guide young enthusiasts in the E-Summit. For the very first time, Entropy-the speaker
+session saw the presence of an international entrepreneur cum coach, Daniel
+Ramamoorthy.
+                        </p>
+                    </div>
+                </div>
+                <div className="timeline">
+                    <span className="icon">2018</span>
+                    <div className="timeline-content">
+                        <h3 className="title">Standup Performance</h3>
+                        <p className="description">
+                        This year, the E-Summit was successfully wrapped with a spine-chilling get-together
+called the E-Gathering. This gathering was addressed by the famous stand-up artist
+Nishant Tanwar and a musical duo performance by two wonderfully talented specially
+abled girls.
+                        </p>
+                    </div>
+                </div>
+                <div className="timeline">
+                    <span className="icon">2018</span>
+                    <div className="timeline-content">
+                        <h3 className="title">Success of Innovation Manthan</h3>
+                        <p className="description">
+                        To promote innovation among students and to provide patents and IP rights for their
+developed prototypes, E-Cell along with CDC organised an innovation expo called
+Innovation Manthan. This exhibition was successful in its objective and has provided
+seed grants worth 25000 INR and patents to top innovations.
                         </p>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </div>
     )
