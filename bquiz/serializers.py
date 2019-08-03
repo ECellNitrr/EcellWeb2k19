0525f3f87ba49e81f3d848807bd3ecccf9b50c93
badducoder <sumit7engineer@gmail.com>
2019-08-03 21:37:08 +0530

    Minor bugs removed

--- bquiz/serializers.py
+++ bquiz/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from .models import Leader
 from django.core.exceptions import ValidationError
-
+from users.models import CustomUser
 
 class LeaderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Leader
         fields = ['questionset', 'user', 'score']
 
-class UserLeaderBoardSerializer(serialzers.ModelSerializer):
+class UserLeaderBoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['username','email','score']
