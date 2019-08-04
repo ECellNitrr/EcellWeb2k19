dd54ea51a06f253a89aba0e88d517acb223c7a62
pratik0204 <pchy.393@gmail.com>
2019-08-04 11:20:13 +0530

    mp embedded

--- frontend/src/Components/Team/modal_2014.js
+++ frontend/src/Components/Team/modal_2014.js
@@ -7,8 +7,8 @@ export default class modal_2014 extends Component {
             <div>
                 <Modal id="TeamModal2014">
                     <div className="text-center font-weight-bold mt-3">
-                        We are looking for <span className="text-primary">Team of 2014</span>. Do you have their address? <br></br><br></br>
-                        If yes, please connect with us at <a className="" href="tel:8094966697">+918094966697</a><br></br>
+                        Looking for <span className="text-primary">Team of 2014</span>? Help us find them too! <br></br><br></br>
+                        Connect with us at <a className="" href="tel:8094966697">+918094966697</a><br></br>
                         <span className="">Email:&nbsp;</span> <a href="mailto:ecell@nitrr.ac.in">ecell@nitrr.ac.in</a>
                     </div>
 
