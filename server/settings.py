caea1f6b9a253a0fc06f3b4c551d6a9d0de93383
anshsrtv <49693160+anshsrtv@users.noreply.github.com>
2019-08-07 23:44:51 +0530

    event_unregister api added (#66)
    * image url , id issue fixed in api
    
    * team api changed
    
    * removed unnecessary images
    
    * local merge
    
    * removed ! from response messages and increased the time delay for resend otp to 20 min
    
    * unregister route added in backend

--- server/settings.py
+++ server/settings.py
@@ -198,21 +198,21 @@ CHANNEL_LAYERS = {
 }
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': 'debug.log',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
