a3a30030549b0467f22505063f59db8a817b0cdd
naveensundar <naveennvrgup@gmail.com>
2019-08-17 20:16:12 +0530

    added views to the router

--- iportal/views.py
+++ iportal/views.py
@@ -7,3 +7,19 @@ from .models import *
 class StartupViewset(ModelViewSet):
     queryset = Startup.objects.filter(approved=True)
     serializer_class = StartupSerializer
+
+
+class JobViewset(ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+        
+class JobApplicationViewset(ModelViewSet):
+    queryset = JobApplication.objects.all()
+    serializer_class = JobApplicationSerializer
+
+
+class LogoViewset(ModelViewSet):
+    queryset = StartupLogo.objects.all()
+    serializer_class = LogoSerializer
+    
