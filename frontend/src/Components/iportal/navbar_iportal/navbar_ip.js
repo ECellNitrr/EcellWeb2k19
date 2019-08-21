8f37caf430a9b48a7d9deba68d1ac76b6a9c4a8f
pratik0204 <pchy.393@gmail.com>
2019-08-21 15:46:20 +0530

    removed unwanted css

--- frontend/src/Components/iportal/navbar_iportal/navbar_ip.js
+++ frontend/src/Components/iportal/navbar_iportal/navbar_ip.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import '../jobs/jobs.css'
-import { elastic as Menu } from 'react-burger-menu'
 
 export default class navbar_ip extends Component {
     render() {
