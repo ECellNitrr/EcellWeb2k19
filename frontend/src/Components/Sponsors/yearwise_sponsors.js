742d3401f4e6f3253c0bb1a35d9a42fde9a09dab
pratik0204 <pchy.393@gmail.com>
2019-08-27 15:13:45 +0530

    removed unnecessary padding in the mobile view of speakers and investors

--- frontend/src/Components/Sponsors/yearwise_sponsors.js
+++ frontend/src/Components/Sponsors/yearwise_sponsors.js
@@ -5,6 +5,7 @@ import Navbar from '../Navbar/navbar';
 import Footer from '../Footer/footer';
 import faxios from '../../axios';
 import Loader from '../api_loader/api_loader'
+import { reverse } from 'dns';
 
 
 
@@ -20,6 +21,7 @@ class Sponsors extends Component{
         this.axios.get('/sponsors/spons_years/').then(res=>{
             
             let data = res.data.spons_year;
+            let data_reverse = data.reverse()
             console.log(data);
             this.setState({
                 spons_years:data,
@@ -43,7 +45,7 @@ class Sponsors extends Component{
                 <Navbar/>
 
                 <div className="header1">OUR SPONSORS</div>
-                <div className="container-fluid ctn11">
+                <div className="container-fluid ctn11" style={{marginTop:"-90px"}}>
                         <div>
                             {this.state.loading ? (<Loader/>):(spons_year_html) }
                         </div>
