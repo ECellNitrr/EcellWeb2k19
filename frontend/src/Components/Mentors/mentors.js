3ff278763952cd33cc34ee39fb8b825fc9974707
pratik0204 <pchy.393@gmail.com>
2019-08-30 19:12:19 +0530

    mentors done

--- frontend/src/Components/Mentors/mentors.js
+++ frontend/src/Components/Mentors/mentors.js
@@ -6,15 +6,16 @@ import Footer from '../Footer/footer';
 import Loader from '../api_loader/api_loader'
 
 class mentors extends Component {
-  axios = faxios();
+ 
   state = {
     mentors: {},
-    loading: true
+    loading: true,
+    mentors_19:[]
   };
 
   componentDidMount(){
-    for(let i=2016; i<=2020;i++){
-      this.axios.get(`/mentors/list/${i}/`).then(res=>{
+    for(let i=2016; i<=2018;i++){
+      faxios().get(`/mentors/list/${i}/`).then(res=>{
         console.log(res)
         let  data = res.data.data
         if(data.length>0){
@@ -28,7 +29,18 @@ class mentors extends Component {
         }
       })
     }
+
+    faxios().get('/mentors/list/2019/').then(res=>{
+        let data = res.data.data
+          console.log(data)
+          this.setState({
+            mentors_19:data
+          })
+      }
+    )
   }
+
+  
 
 
   render() {
@@ -39,7 +51,7 @@ class mentors extends Component {
         let mentors = this.state.mentors[year]
         mentors = mentors.map(mentor => 
             <div className="individual_mentors" key={mentor.id}>
-                <div><img className="mentors_pic shadow-lg p-3 mb-5 bg-white rounded" src={mentor.profile_pic_url} alt={mentor.name}/></div>
+                <div className="mentors_pic_div"><img className="mentors_pic shadow-lg p-3 mb-5 bg-white rounded" src={mentor.profile_pic_url} alt={mentor.name}/></div>
                 <h3 className="mentors_name">{mentor.name}</h3>
                 <p className="center2">{mentor.detail}</p>
             </div>
@@ -51,12 +63,40 @@ class mentors extends Component {
         mentors_html.push(yearwise_html)
     }
 
+    let mentor19Html= []
+
+    const mentors_19 = this.state.mentors_19
+
+    console.log(mentors_19)
+
+    let mentors_19_html = mentors_19.map(mentor => 
+      <div className="individual_mentors" key={mentor.id}>
+          <div className="mentors_pic_div"><img className="mentors_pic shadow-lg p-3 mb-5 bg-white rounded" src={mentor.profile_pic_url} alt={mentor.name}></img></div>
+          <h3 className="mentors_name">{mentor.name}</h3>
+          <p className="center2">{mentor.detail}</p>
+      </div>
+      )
+
+      const mentor19 = <div className="flex-containerr">
+        {mentors_19_html}
+      </div>
+
+      mentor19Html.push(mentor19)
+  
+
     return (
       <div className="mentors">
         <Navbar />
-        <div className="header4">PREVIOUS MENTORS</div>          
+        
+
+            <div className="header4">MENTORS 2019</div>          
             <div className="container-fluid ctn13">
-              {this.state.loading? (<div style={{marginTop:"20%"}}><Loader/></div>):mentors_html}
+              {this.state.loading? (<div style={{marginTop:"20%"}}><Loader/></div>):mentor19Html}
+            </div>
+
+            <div className="header44">PREVIOUS MENTORS</div>          
+            <div className="container-fluid">
+              {this.state.loading? (<div style={{marginTop:"10%"}}><Loader/></div>):mentors_html}
             </div>
                   
         <Footer/>
