0ba59e442dbce01d699d34af82bd71748fecc908
anshsrtv <ansh_srtv@outlook.com>
2020-04-20 10:40:57 +0530

    education model updated

--- iportal/models.py
+++ iportal/models.py
@@ -169,13 +169,60 @@ class Organisation(models.Model):
 
 
 class ApplicantEducation(models.Model):
+    course_choices =(
+        ('B Tech','Bachelor of Technology'),
+        ('M Tech','Master of Technology'),
+        ('MBA','Master of Business Administration'),
+        ('B Sc','Bachelor of Science'),
+        ('B Arch','Bachelor of Architecture'),
+        ('Other','Other')
+    )
+    branch_choices =(
+        ('Biomed','Bio Medical Engineering'),
+	    ('Biotech','Bio Technology Engineering'),
+	    ('Chemical','Chemical Engineering'),
+	    ('Civil','Civil Engineering'),
+	    ('CSE','Computer Science Engineering'),
+    	('ELEX','Electronics and communication Engineering'),
+    	('Electrical','Electrical Engineering'),
+    	('IT','Information Technology'),
+    	('Mech','Mechanical Engineering'),
+    	('Meta','Metallurgical and Materials Engineering'),
+    	('Mining','Mining Engineering'),
+        ('Finance','Finance'),
+        ('Marketing','Marketing'),
+        ('HRM','Human Recourse Management'),
+        ('IB','International Business'),
+        ('OM','Operation Management'),
+        ('SCM','Supply Chain Management'),
+        ('RM','Rural Management'),
+        ('ABM','Agri Business Management'),
+        ('HCM','Health Care Management'),
+        ('LA','Landscape Architecture'),
+        ('AC','Architectural Conservation'),
+        ('HA','Housing Architecture'),
+        ('UP','Urban Planning'),
+        ('RP','Regional Planning'),
+        ('HM','Hotel Management'),
+        ('AS','Aernautical Science'),
+        ('Medical','Medical Science'),
+        ('Biology','Biologcal Science'),
+        ('AVE','Animation and Visual Effects'),
+        ('Chemistry','Applied Chemistry'),
+        ('Maths','Applied Mathemartics'),
+        ('Physics','Applied Physics'),
+        ('Other','Other'),
+    )
+
     applicant = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     study_program = models.CharField(max_length=200)
     institution = models.ForeignKey(Institute, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
     result = models.CharField(max_length=200, null=True, blank=True)
-    courses = models.TextField( null=True, blank=True)
+    course = models.CharField(max_length=100, choices= course_choices)
+    branch = models.CharField(max_length=100, choices= branch_choices)
+    semester = models.PositiveIntegerField() #max=10
 
 class ApplicantExperience(models.Model):
     applicant = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
