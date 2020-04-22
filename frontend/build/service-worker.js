1f9908031784a1c25a090c0309dfec4ce806e2fd
Naveen Sundar <naveennvrgup@gmail.com>
2020-04-22 16:08:04 +0530

    built with extra fields

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c2bd8b2cc599a4b26fb29ccb6ca841e.js"
+  "/precache-manifest.f2f6b877e87112f326ceaa8d97931c1d.js"
 );
 
 self.addEventListener('message', (event) => {
