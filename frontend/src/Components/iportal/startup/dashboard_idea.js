b55b4895eb8f4c26a3fab63e9f255275f09474c4
Kaushik Mishra <kmishra27.km@gmail.com>
2020-04-22 22:39:28 +0530

    dashboard made for multiple ideas

--- frontend/src/Components/iportal/startup/dashboard_idea.js
+++ frontend/src/Components/iportal/startup/dashboard_idea.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react'
 import Parser from 'html-react-parser'
 import faxios, { baseURL } from '../../../axios'
 import './dashboard.scss'
-
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
@@ -43,61 +43,59 @@ class ideadashboard extends Component {
         return(
 
             <div className="row" key={idea.id}>
-            <div className="col-md-12">
-
-                    <div className="form-group">
-                        <label className='font-weight-bold'>Idea in a Nutshell : </label>
-                        <span>{idea.idea_in_a_nut_shell}</span>
-                    </div>
-
-                    {/* <div className="form-group">
-                        <label className='font-weight-bold'>Ideator Name : </label>
-                        <span className="font-weight-bold">{idea.name}</span>
-                    </div> */}
-
-                    {/* <div className="form-group">
-                        <label className='font-weight-bold'>Ideator Designation : </label>
-                        <span className="font-weight-bold">{idea.ideator_designation}</span>
-                    </div> */}
-
+                <div className="col-md-12">
                     
-                    
-                    <div className="form-group">
-                        <label className='font-weight-bold'>Description : </label>
-                        <div dangerouslySetInnerHTML={{ __html: idea.describe_idea }}></div>
-                    </div>
-                    
-                    <div className="form-group">
-                        <label className='font-weight-bold'>Innovation in this : </label>
-                        <div dangerouslySetInnerHTML={{ __html: idea.innovation_in_this}}></div>
+                    <div class="card">
+                        <h5 class="card-header font-weight-bold">{idea.idea_in_a_nut_shell}</h5>
+                        <div class="card-body">
+
+                            <div className="mb-2"><i className="font-weight-bold">Description :</i></div>
+                            <div dangerouslySetInnerHTML={{ __html: idea.describe_idea }}></div>
+
+                            <div className="mb-2"><i className="font-weight-bold">Innovation in this :</i></div>
+                            <div dangerouslySetInnerHTML={{ __html: idea.innovation_in_this}}></div>
+
+                            <div className="mb-2"><i className="font-weight-bold">End Product :</i>
+                            </div>
+                            <span>{idea.end_product}</span>
+
+                            <div className="mb-2" style={{paddingTop:"10px"}}><i className="font-weight-bold">Beneficiaries :</i>
+                            </div>
+                            <span>{idea.beneficiaries}</span>
+
+                            <div className="form-group">
+                            <label className='font-weight-bold' style={{paddingTop:"10px"}}>Idea Approved : </label>
+                                <span style={{paddingLeft:"10px"}}>{idea.idea_approved ? <i className="fa fa-check text-success"></i> : <i className="fa fa-times text-danger"></i>}</span>
+                                <div>
+                                    <em>{idea.idea_approved ? null : "(You will receive confirmation by E-mail and SMS once the verification is complete)"}</em>
+                                </div>
+
+                            </div>
+
+                             <div>
+                                {idea.idea_approved ? <Link to="/internship/startup/register/" className='btn float-right font-weight-bold btn-primary'>Startup Profile</Link> : <Link to="/internship/startup/register/" className='btn float-right font-weight-bold btn-primary disabled'>Startup Profile</Link>}
+                                {idea.can_hire_interns ? <Link to="/internship/startup/openings/" className='btn float-right font-weight-bold btn-primary'>Hire Interns</Link> : <Link to="/internship/startup/openings/" className='btn float-right font-weight-bold btn-primary disabled'>Hire Interns</Link>}
+
+                             </div>
+                        </div>
                     </div>
 
-                    <div className="form-group">
-                        <label className='font-weight-bold'>End Product : </label>
-                        <span>{idea.end_product}</span>
-                    </div>
-
-                    <div className="form-group">
-                        <label className='font-weight-bold'>Beneficiaries : </label>
-                        <span>{idea.beneficiaries}</span>
-                    </div>  
-                    
-                    <div className="form-group">
-                        <label className='font-weight-bold'>Idea Approved : </label>
-                        <span>{idea.idea_approved ? <i className="fa fa-check text-success"></i> : <i className="fa fa-times text-danger"></i>}</span>
-                    </div>
-
-                    </div>      
-                </div>
+                </div>      
+            </div>
         )
     })
 
     return (
         <div className="pb-2"  style={{background:"lightgray",paddingTop:"",marginBottom:"-50px"}}>
             <div className='container jumbotron hoverable'>
+                <div style={{float:"left"}}>
+                    <button onClick={() => this.props.history.push('/internship/submit_idea/')} className="btn font-weight-bold align-self-center btn-primary">Submit More Ideas</button>
+                </div>
+            <br/><br/>
             <div>
                 <div className="d-flex my-5">
                 <h1 className="text-center flex-grow-1 font-weight-bold">Idea Dashboard</h1>
+
                 {/* <button onClick={() => this.props.history.push('/internship/submit_idea/edit_idea/')} className="btn btn-danger font-weight-bold">Edit</button> */}
                 {/* {idea.can_hire_interns?null:<Fragment>
                     <button onClick={() => this.props.history.push('/internship/startup/register')} className="btn btn-success font-weight-bold">Register Startup</button>
