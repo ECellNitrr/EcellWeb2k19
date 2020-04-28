408fdf036c5b04a2e5dba837f72bc842540d0005
Naveen Sundar <naveennvrgup@gmail.com>
2020-04-28 23:10:34 +0530

    bug fix django signal

--- iportal/signals.py
+++ iportal/signals.py
@@ -8,22 +8,23 @@ from django.template.loader import render_to_string
 
 @receiver(pre_save, sender=Startup)
 def email_update_startup(sender, instance=None, **kwargs):
-    previous = Startup.objects.get(id=instance.id)
-    subject='An Update from Career Development Cell NIT Raipur'
+    try:
+        previous = Startup.objects.get(id=instance.id)
+        subject='An Update from Career Development Cell NIT Raipur'
 
-    if instance.idea_approved!=previous.idea_approved:
-        if instance.idea_approved:
-            html_content=render_to_string('idea_approved_mail.html',{'startup_name': instance.name})
-            mail.delay(subject,html_content,instance.email)
-        else:
-            html_content=render_to_string('idea_rejected_mail.html',{'startup_name': instance.name})
-            mail.delay(subject,html_content,instance.email)
-    elif instance.can_hire_interns!=previous.can_hire_interns:
-        if instance.can_hire_interns:
-            html_content=render_to_string('hire_intern_mail.html',{'startup_name': instance.name})
-            mail.delay(subject,html_content,instance.email)
-        else:
-            html_content=render_to_string('revoke_hire_mail.html',{'startup_name': instance.name})
-            mail.delay(subject,html_content,instance.email)
-
-    
+        if instance.idea_approved!=previous.idea_approved:
+            if instance.idea_approved:
+                html_content=render_to_string('idea_approved_mail.html',{'startup_name': instance.name})
+                mail.delay(subject,html_content,instance.email)
+            else:
+                html_content=render_to_string('idea_rejected_mail.html',{'startup_name': instance.name})
+                mail.delay(subject,html_content,instance.email)
+        elif instance.can_hire_interns!=previous.can_hire_interns:
+            if instance.can_hire_interns:
+                html_content=render_to_string('hire_intern_mail.html',{'startup_name': instance.name})
+                mail.delay(subject,html_content,instance.email)
+            else:
+                html_content=render_to_string('revoke_hire_mail.html',{'startup_name': instance.name})
+                mail.delay(subject,html_content,instance.email)
+    except:
+        pass
