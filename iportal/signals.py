d11e3b6b02e8738ef90b10f4f851216e07749fa2
priyanshisharma <priyanshi417@gmail.com>
2020-05-26 02:39:31 +0530

    Added mailing

--- iportal/signals.py
+++ iportal/signals.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Startup
+from .models import Startup, JobApplication
 from .tasks import mail
 from django.template.loader import render_to_string
 
@@ -28,3 +28,35 @@ def email_update_startup(sender, instance=None, **kwargs):
                 mail.delay(subject,html_content,instance.email)
     except:
         pass
+
+    
+@receiver(post_save, sender=JobApplication)
+def job_application_status_update(sender, instance=None, **kwargs):
+    try:
+        after = JobApplication.objects.get(id = instance.id)
+        subject = 'Update Regarding your Internship Application' 
+    
+    if instance.status!=after.status:
+        if instance.status == 'RJD':
+            html_content = render_to_string('PND_to_RJD.html',{'job_name' : instance.job.name , 'startup_name' : instance.job.startup.name})
+            mail.delay(subject,html_content,instance.applicant.email)
+        
+        elif instance.status == 'HRD':
+            html_content = render_to_string('PND_to_HRD.html',{'job_name' : instance.job.name , 'startup_name' : instance.job.startup.name})
+            mail.delay(subject,html_content,instance.applicant.email)
+        
+        elif instance.status == 'URV':
+            html_content = render_to_string('PND_to_URV.html',{'job_name' : instance.job.name , 'startup_name' : instance.job.startup.name})
+            mail.delay(subject,html_content,instance.applicant.email)
+
+@receiver(post_save, sender=JobApplication)
+def user_created(sender, instance, created, *args, **kwargs):
+    application = instance
+    if created:
+        subject_applicant ='Your internship application has been received!'
+        html_content_applicant = render_to_string('Applicant_acknowledgement.html',{'job_name' : instance.job.name , 'startup_name' : instance.job.startup.name})
+        mail.delay(subject_applicant,html_content_applicant,instance.applicant.email)
+        
+        subject_startup ='An application for your internship position has been received!'
+        html_content_startup = render_to_string('startup_acknowledgement.html',{'job_name' : instance.job.name})
+        mail.delay(subject_startup,html_content_startup,instance.job.startup.email)           
