8dc8666db3901867a7565d51ade3a1ab02e80a27
Kaushik Mishra <kmishra27.km@gmail.com>
2021-01-12 00:25:11 +0530

    ECell 20 team added and added Esummit 19 images

--- frontend/src/Components/Gallery/gallery.js
+++ frontend/src/Components/Gallery/gallery.js
@@ -15,6 +15,7 @@ class gallery extends Component{
                 
                 <div className="container-fluid" style={{maxWidth:"700px",paddingTop:"200px"}}>
                     <div className="header66">Gallery</div>
+                    <div className="gal-link"><Link to="gallery/E-Summit'19"><button className="btn lgtabs lgback" >GALLERY OF E-SUMMIT'19</button></Link></div> 
                     <div className="gal-link"><Link to="gallery/E-Summit'18"><button className="btn lgtabs lgback" >GALLERY OF E-SUMMIT'18</button></Link></div> 
                     <div className="gal-link"><Link to="gallery/E-Summit'17"><button className="btn lgtabs lgback" >GALLERY OF E-SUMMIT'17</button></Link></div> 
                     <div className="gal-link"><Link to="gallery/E-Summit'16"><button className="btn lgtabs lgback" >GALLERY OF E-SUMMIT'16</button></Link></div> 
